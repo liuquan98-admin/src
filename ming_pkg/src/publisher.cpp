3cd985d98c82f03c1200f12dbc76a555d49d399e
530637626@qq.com <530637626@qq.com>
2023-07-28 14:03:28 +0900

    關於自定義MSG的提交

---
+++ ming_pkg/src/publisher.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <ming_pkg/sttay.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    //1創建節點
+    ros::init(argc,argv,"talker_person");
+
+    //2.创建 ROS 句柄
+    ros::NodeHandle nh;
+    //3.创建发布者对象
+    ros::Publisher pub = nh.advertise<ming_pkg::sttay>("/count",10);
+    
+    ros::Rate r(1);
+    
+    int number_conunt=0;
+   
+    // ming_pkg::sttay p; //申明自定義消息
+    // //使用自定義數據進行填充
+    // p.name="sucan";
+    // p.age=18;
+    // p.height=178;
+    
+    while (ros::ok())
+    {
+        ming_pkg::sttay p;
+        p.name="robot";
+        p.deivce_id=number_conunt%5;
+        p.percent=rand()/double(RAND_MAX);
+        pub.publish(p);
+        
+        ros::spinOnce();
+        r.sleep();
+        number_conunt++;
+    }
+    
+    return 0;
+}
