7cc77d919375be77a776006e9fa68a4f1b83056c
530637626@qq.com <530637626@qq.com>
2023-08-02 03:37:36 +0900

    少寫了個對象名！

--- ming_pkg/src/subscrlist.cpp
+++ ming_pkg/src/subscrlist.cpp
@@ -3,18 +3,17 @@
 #include <stdio.h>
 
 void number_call(const ming_pkg::sttay::ConstPtr& msg){
- ROS_INFO("name is [%s]",msg->name);
- ROS_INFO("deivce is [%d]",msg->deivce_id);
- ROS_INFO("percent is [%f]",msg->percent);
+ ROS_INFO("name is [%s],deivce is[%d],percent is [%f]",msg->name.c_str(),msg->deivce_id,msg->percent);
+
 
 
 }
-
 int main(int argc, char  *argv[])
 {
     ros::init(argc,argv,"subsrc_node");
     ros::NodeHandle ns;
-    ns.subscribe<ming_pkg::sttay>("/count",10,number_call);
+    ros:: Subscriber number_subscriber=ns.subscribe<ming_pkg::sttay>("/count",10,number_call);
     ros::spin();
+  
     return 0;
 }
