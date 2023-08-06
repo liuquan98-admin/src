16c9b5c0262bd05d53ca5776b4d3d877ba18a54b
530637626@qq.com <530637626@qq.com>
2023-08-07 00:35:49 +0900

    添加客戶端代碼

--- ming_pkg/src/service_node.cpp
+++ ming_pkg/src/service_node.cpp
@@ -1,23 +1,27 @@
 #include <ros/ros.h>
 #include <sstream>
 #include <ming_pkg/mysrc.h>
+ using namespace std;
+ 
+//收到請求後執行回調函數
 bool service_callback(ming_pkg::mysrc::Request &req,
                       ming_pkg::mysrc::Response &res)
 {
   std::stringstream ss;
-  ROS_INFO("From client get: [%d],[%d]",req.num_a,req.num_b);
-  int sum=req.num_a+req.num_b;
-  ss<<"Sum is "<<sum;
+  ss<<"Received Here";
   res.name=ss.str();
+  ROS_INFO("Form Client[%s],form Service [%s]",req.num_a.c_str(),res.name.c_str());
   return true;
 }
 
 int main(int argc, char  *argv[])
 
 {
-    ros::init(argc,argv,"service_node");
+    ros::init(argc,argv,"service_node");//service_node節點
     ros::NodeHandle ns;
-    ros::ServiceServer service=ns.advertiseService("service_sum",service_callback);
+    
+    ros::ServiceServer service=ns.advertiseService("service_node",service_callback);
+    
     ROS_INFO("ready to Service data from client........");
     ros::spin();
     return 0;
